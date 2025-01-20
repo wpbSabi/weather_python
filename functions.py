fa62aafb23b061f739abc17c1c43f727dbfacd38
Sabi Horvat <sabolch.horvat@gmail.com>
2025-01-19 16:36:36 -0800

    added function view_station_date_ranges() to functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-def update_data(new_data_path: str, 
-                existing_data_path: str,
-                overwrite: str='yes') -> pd.DataFrame:
+
+def update_data(
+    new_data_path: str, existing_data_path: str, overwrite: str = "yes"
+) -> pd.DataFrame:
     """
     1. Import new data
     2. Check the format of the data
@@ -30,7 +31,34 @@ def update_data(new_data_path: str,
     combined = pd.concat([new, existing]).drop_duplicates()
 
     # Overwrite the existing data
-    if overwrite=='yes':
+    if overwrite == "yes":
         combined.to_csv(existing, index=False)
 
     return combined
+
+
+def view_station_date_ranges(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns a data frame that shows the min and max date for each
+        station in the data.
+
+    Args:
+        df (data frame) - raw weather data
+
+    Returns:
+        return_df (data frame) - filtered data frame that meets the criteria
+
+    """
+    station_list = pd.DataFrame()
+    for station in df.STATION.unique():
+        a = df[df["STATION"] == station]
+        a = a.assign(min_date=pd.to_datetime(min(a["DATE"])))
+        a = a.assign(max_date=pd.to_datetime(max(a["DATE"])))
+        station_list = pd.concat([station_list, a])
+    return_df = (
+        station_list[["STATION", "NAME", "min_date", "max_date"]]
+        .drop_duplicates()
+        .sort_values("STATION")
+        .reset_index(drop=True)
+    )
+    return return_df
