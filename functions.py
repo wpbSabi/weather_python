72ddf3c64bc8b22e4aad013f07dde546272db859
Sabi Horvat <sabolch.horvat@gmail.com>
2025-12-31 08:01:36 -0800

    added metric parameter to view_station_date_ranges()

--- functions.py
+++ functions.py
@@ -6,54 +6,23 @@ import pandas as pd
 import seaborn as sns
 
 
-def update_data(
-    new_data_path: str, existing_data_path: str, overwrite: str = "yes"
-) -> pd.DataFrame:
-    """
-    1. Import new data
-    2. Check the format of the data
-    3. Append the data to existing data
-    4. Remove duplicates
-    5. Overwrite the existing data if overwrite = 'yes'
-
-    Args:
-        new_data_path (string) - used to read the CSV file in the path
-        existing_data_path (string) - used to read the CSV file in the path
-        overwrite (string) - the default option to overwrite can be disabled
-
-    Returns:
-        df (data frame) - updated data frame with the results
-        Also overwrites the existing_data_path
-    """
-    # Import new data
-    new = pd.read_csv(new_data_path)
-    existing = pd.read_csv(existing_data_path)
-
-    # Check that the columns match between the data frames
-    assert (new.columns == existing.columns).all()
-
-    # Append the data to existing data and remove duplicates
-    combined = pd.concat([new, existing]).drop_duplicates()
-
-    # Overwrite the existing data
-    if overwrite == "yes":
-        combined.to_csv(existing_data_path, index=False)
-
-    return combined
-
-
-def view_station_date_ranges(df: pd.DataFrame) -> pd.DataFrame:
+def view_station_date_ranges(df: pd.DataFrame, metric: str) -> pd.DataFrame:
     """
     Returns a data frame that shows the min and max date for each
         station in the data.
 
     Args:
         df (data frame) - raw weather data
+        metric (string) - choose a metric in the weather data: TMAX, TMIN, etc.
 
     Returns:
         return_df (data frame) - filtered data frame that meets the criteria
 
     """
+
+    # Filter out stations that do not have non-null data for the metric
+    df = df[~df[metric].isnull()]
+
     station_list = pd.DataFrame()
     for station in df.STATION.unique():
         a = df[df["STATION"] == station]
